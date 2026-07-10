39683199f149f61d3f07840965460d0d21c542a7
JeffryLee <z.li@princeton.edu>
2026-07-09 23:56:20 -0700

    Nerf private ticket VP by an additional 20%
    Scale all trip ticket completion points (and matching penalties) down
    another step so hotel paths compete more with pure flight/ticket play.

--- points-odyssey/js/ui.js
+++ points-odyssey/js/ui.js
@@ -12,10 +12,10 @@ import {
   ACHIEVEMENTS,
   GAME_CONFIG,
   listFlightOptions,
-} from './game.js?v=goldpath1';
-import { BANKS, HOTELS, AIRLINES, getRoute, STRATEGY_TIPS } from './data.js?v=goldpath1';
-import { playBotActions } from './bot.js?v=goldpath1';
-import { initMusicUI, playTrack, ensureMusic } from './music.js?v=goldpath1';
+} from './game.js?v=tixnerf3';
+import { BANKS, HOTELS, AIRLINES, getRoute, STRATEGY_TIPS } from './data.js?v=tixnerf3';
+import { playBotActions } from './bot.js?v=tixnerf3';
+import { initMusicUI, playTrack, ensureMusic } from './music.js?v=tixnerf3';
 
 const game = new Game();
 let setupSelections = [];
